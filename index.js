89a712c901373fbcb97d9d184784083e7196bf45
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-10-12 08:58:46 -0400

    rwgwR

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const text = document.querySelector("#text"); //specify id or class inside ()
 console.log(button, text);
  */
+//The Document Object Model, or DOM, is essentially the elements you write into your HTML. DOMSelectors allow you to easily access your HTML in JS.
+
+// DOMSelectors are objects that hold access to the DOM that you can call upon easily at any point in your code.
 const DOMSelectors = {
     button:document.getElementById("btn"),
     text:document.querySelector("#text"),
@@ -9,5 +12,6 @@ const DOMSelectors = {
     point:document.querySelectorAll(".point"), //gets all instances of this
     pointTwo:document.getElementById("pointTwo"),
 };
+//console.log(DOMSelectors.points[0]); // this returns the first ever element in the document with class 'point'
 //console.log(DOMSelectors.button) //can get specific element
 console.log(DOMSelectors);
