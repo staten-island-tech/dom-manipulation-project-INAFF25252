fbc814f65fd9c7316d036955b593ca001a1093ac
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-10-20 09:24:51 -0400

    WHA TITLE

--- dom.js
+++ dom.js
@@ -8,9 +8,11 @@ console.log(button, text);
 const DOMSelectors = {
     form: document.querySelector("#form"),
     input1: document.querySelector(".first-name"),
-    input2: document.querySelector(".old"),
-    h2s: document.querySelectorAll("h2"),
-    h3s: document.querySelectorAll("h3"),
+    input2: document.querySelector(".last-name"),
+    input3: document.querySelector(".old"),
+    h2: document.querySelectorAll(".first"),
+    h2last:document.querySelectorAll(".last"),
+    h3: document.querySelectorAll("h3"),
     button:document.getElementById("#btn"),
     text:document.querySelector("#text"),
     empty:document.querySelector(".nothing"),
@@ -20,12 +22,27 @@ const DOMSelectors = {
 //console.log(DOMSelectors.points[0]); // this returns the first ever element in the document with class 'point'
 //console.log(DOMSelectors.button) //can get specific element
 console.log(DOMSelectors);
+function makeProfile() {
+const profile = (`Hi, ${document.querySelectorAll(".first-name")} ${document.querySelectorAll(".last-name")} ${document.querySelectorAll(".old")}`)
+return profile;
+}
+function make(profile) {
+
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.input1.value, DOMSelectors.input2.value);
-    DOMSelectors.h2s.forEach((el) => el.textContent=DOMSelectors.input1.value);
-    DOMSelectors.h3s.forEach((el) => el.textContent=DOMSelectors.input2.value);
+    DOMSelectors.h2.forEach((el) => el.textContent=DOMSelectors.input1.value);
+    DOMSelectors.h2last.forEach((el) => el.textContent=DOMSelectors.input2.value);
+    DOMSelectors.h3.forEach((el) => el.textContent=DOMSelectors.input3.value);
+    document.querySelector(".gallery").insertAdjacentHTML("afterbegin", `<div class="card"><div class = "card-title">${DOMSelectors.firstName.value}</div></div>`);
+    const profile = makeProfile();
+    //clearFields()
+//remove button
 });
+};
+make();
+
+
 
 /* let x = prompt("Insert First Name");
 let y = prompt("Insert Last Name");
